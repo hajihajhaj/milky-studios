49989455e8e5274cab5178d46be5b81578731023
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2025-04-27 19:57:38 -0400

    basic dialogue
    arrow to skip text and to go to next line, exit button to get out of dialogue (finally)

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -7,61 +7,101 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
+    public GameObject nextLineArrow;
+    public GameObject exitButton;
+    public GameObject dialogueBox;
 
     private int index;
+    private bool isTyping = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        textComponent.text = string.Empty;
-        StartDialogue();
+        if (dialogueBox != null) dialogueBox.SetActive(false);
+        if (nextLineArrow != null) nextLineArrow.SetActive(true);
+        if (exitButton != null) exitButton.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            if(textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
-    }
+        if (dialogueBox != null) dialogueBox.SetActive(true);
 
-    void StartDialogue()
-    {
         index = 0;
+        textComponent.text = string.Empty;
+
+        if (exitButton != null) exitButton.SetActive(false);
+
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
+        isTyping = true;
+
+        if (nextLineArrow != null) nextLineArrow.SetActive(true);
+
         foreach (char c in lines[index].ToCharArray())
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+
+        isTyping = false;
+
+        if (index < lines.Length - 1)
+        {
+            if (nextLineArrow != null) nextLineArrow.SetActive(true);
+        }
+        else
+        {
+            if (nextLineArrow != null) nextLineArrow.SetActive(false);
+            if (exitButton != null) exitButton.SetActive(true);
+        }
     }
 
-    void NextLine() 
-    { 
-       if (index<lines.Length - 1) 
-       {
-        index++;
-        textComponent.text = string.Empty;
-        StartCoroutine(TypeLine());
-       }
-       else
-       {
-            gameObject.SetActive(false);
-       }
+    public void OnNextLineClicked()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            textComponent.text = lines[index];
+            isTyping = false;
 
-                       
+            if (index < lines.Length - 1)
+            {
+                if (nextLineArrow != null) nextLineArrow.SetActive(true);
+            }
+            else
+            {
+                if (nextLineArrow != null) nextLineArrow.SetActive(false);
+                if (exitButton != null) exitButton.SetActive(true);
+            }
+        }
+        else
+        {
+            NextLine();
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            CloseDialogue();
+        }
+    }
+
+    public void CloseDialogue()
+    {
+        textComponent.text = "";
+
+        if (dialogueBox != null) dialogueBox.SetActive(false);
+        if (exitButton != null) exitButton.SetActive(false);
+        if (nextLineArrow != null) nextLineArrow.SetActive(false);
     }
 }
